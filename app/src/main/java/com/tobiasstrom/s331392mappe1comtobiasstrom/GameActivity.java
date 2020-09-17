889a4218ee1f2f273359fe4f83f78b1dedd10cdd
Karolinio <36259145+Karolinio@users.noreply.github.com>
2020-09-17 19:21:45 +0200

    statestikk, oppdatering av språket, oppbevaring av modalen
    statesikk blir lagret til sharedpreferances, språket blir oppdatert inn i preferences og modalen som vises på slutten av spillet kan ikke klikkes bort og den obevares selvom mobilen roteres.

--- app/src/main/java/com/tobiasstrom/s331392mappe1comtobiasstrom/GameActivity.java
+++ app/src/main/java/com/tobiasstrom/s331392mappe1comtobiasstrom/GameActivity.java
@@ -3,11 +3,10 @@ package com.tobiasstrom.s331392mappe1comtobiasstrom;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
@@ -16,7 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,7 +30,7 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
     private String[] questions;
     private String[] answers;
     private ArrayList<Integer> selectedQuestions = new ArrayList();
-    private int numberOfQuestions = 10; //default verdi dersom den fantes ikke i preferanser (just in case)
+    private int numberOfQuestions = 5; //default verdi dersom den finnes ikke i sharedPreferences
     private int whichQuestion = 0;
     TextView txt_game_question;
     TextView txt_right_awser;
@@ -39,11 +38,14 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
     private int questionNumber = 0;
     private int rightAwser = 0;
     private int wrongAwser = 0;
+    private int totalRightAnswer = 0;
+    private int totalWrongAnswer = 0;
     Dialog myDialog;
     Button btnClose;
     TextView txt_result_of, txt_result;
     ArrayList<Statistics> statistics = new ArrayList<>();
-
+    SharedPreferences sharedPreferences;
+    View view;
 
     private static final String STATE_NUMBEROFQUESTION = "NumberOfQuestion";
     private static final String STATE_WHICHQUESTION = "WhitsQuestion";
@@ -52,28 +54,52 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
     private static final String STATE_RIGHTAWSER = "RightAwser";
     private static final String STATE_WRONGAWSER = "WrongAwser";
 
+    //metoden som prøver å hente antall spørsmål fra sharedPreferences, gjør ingenting dersom den feiler
     private void setAmountOfQuestions(String preferance) {
         try {
             numberOfQuestions = Integer.parseInt(preferance);
         } catch (Exception e) {
             //gjør ingenting dersom default verdi er deklarert
         }
-
     }
 
-    public void settland(String landskode) {
+    //metoden som prøver å hente verdier fra sharedPreferences, setter verdi til 0 dersom den feiler
+    private int getStats(SharedPreferences sharedPreferences, String key) {
+        int value;
+        try {
+            value = sharedPreferences.getInt(key, 0);
+        } catch (Exception e) {
+            value = 0;
+        }
+        return value;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DialogFragment dialog = new MyDialog();
+        dialog.show(getFragmentManager(),"Avslutt");
+    }
+
+
+    public void setLang(String prefix) {
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
         Configuration cf = res.getConfiguration();
-        cf.setLocale(new Locale(landskode));
+        //setLocale krever api lvl 17 (current definert api lvl er 15) Dette er en av forslagene til AndroidStudio
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            cf.setLocale(new Locale(prefix));
+        }
         res.updateConfiguration(cf,dm);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        settland(sharedPreferences.getString("languagePref",""));
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        setLang(sharedPreferences.getString("languagePref",""));
         setAmountOfQuestions(sharedPreferences.getString("questionPref",""));
+
+        totalRightAnswer = getStats(sharedPreferences,"stats_right");
+        totalWrongAnswer = getStats(sharedPreferences, "stats_wrong");
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
@@ -120,12 +146,17 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
         questions = getResources().getStringArray(R.array.questions);
         answers = getResources().getStringArray(R.array.answers);
 
-        txt_game_question.setText(questions[questionNumber]+ " =");
-        txt_right_awser.setText(rightAwser+"");
-        txt_wrong_awser.setText(wrongAwser+"");
+        //printer verdier til brukeren
+        txt_game_question.setText(String.valueOf(questions[questionNumber]));
+        txt_right_awser.setText(String.valueOf(rightAwser));
+        txt_wrong_awser.setText(String.valueOf(wrongAwser));
+
+
 
         nextQuestion();
+
         myDialog = new Dialog(this);
+        myDialog.setCancelable(false);
     }
 
     public void btnExitGame(View view) {
@@ -142,9 +173,10 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
     public void onYesClick() {
         finish();
     }
+
     @Override
     public void onNoClick() {
-        return;
+        //return;
     }
 
     public void btnCheck(View view){
@@ -157,12 +189,12 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
 
             if (answers[questionNumber].equals(youAnswerd)){
                 rightAwser++;
-                txt_right_awser.setText(rightAwser+"");
+                txt_right_awser.setText(String.valueOf(rightAwser));
                 newNumber.setText("");
             }
             else {
                 wrongAwser++;
-                txt_wrong_awser.setText(wrongAwser+"");
+                txt_wrong_awser.setText(String.valueOf(wrongAwser));
                 newNumber.setText("");
             }
             youAnswerd = "";
@@ -171,11 +203,11 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
         else{
             if (answers[questionNumber].equals(newNumber.getText().toString())){
                 rightAwser++;
-                txt_right_awser.setText(rightAwser+"");
+                txt_right_awser.setText(String.valueOf(rightAwser));
             }
             else {
                 wrongAwser++;
-                txt_wrong_awser.setText(wrongAwser+"");
+                txt_wrong_awser.setText(String.valueOf(wrongAwser));
             }
             //txt_game_question.setText("Du er ferdig");
             //Context context = getApplicationContext();
@@ -183,6 +215,16 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
             //int duration = Toast.LENGTH_SHORT;
 
             showPopup(view);
+
+            //oppdatere session statistikk
+            totalRightAnswer += rightAwser;
+            totalWrongAnswer += wrongAwser;
+
+            //lagre session statistikk til sharedPreferences
+            sharedPreferences.edit().putInt("stats_right", totalRightAnswer).apply();
+            sharedPreferences.edit().putInt("stats_wrong", totalWrongAnswer).apply();
+
+
             Statistics anser = new Statistics(rightAwser,numberOfQuestions);
             Statistics statistics1 = new Statistics(2,6);
             statistics.add(statistics1);
@@ -201,7 +243,7 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
             if (!selectedQuestions.contains(number)) {
                 selectedQuestions.add(number);
                 questionNumber = number;
-                txt_game_question.setText(questions[questionNumber] + " =");
+                txt_game_question.setText(String.valueOf(questions[questionNumber]));
                 whichQuestion++;
                 approved = true;
             }
@@ -229,9 +271,15 @@ public class GameActivity extends AppCompatActivity implements MyDialog.DialogCl
         questionNumber = savedInstanceState.getInt(STATE_QUESTIONNUMBER);
         rightAwser = savedInstanceState.getInt(STATE_RIGHTAWSER);
         wrongAwser = savedInstanceState.getInt(STATE_WRONGAWSER);
-        txt_game_question.setText(questions[questionNumber]+" =");
-        txt_right_awser.setText(rightAwser+"");
-        txt_wrong_awser.setText(wrongAwser+"");
+        txt_game_question.setText(String.valueOf(questions[questionNumber]));
+        txt_right_awser.setText(String.valueOf(rightAwser));
+        txt_wrong_awser.setText(String.valueOf(wrongAwser));
+
+        //for opp modalen dersom mobilen har blitt rotert
+        //dette skjer kun dersom spillet er ferdig
+        if (whichQuestion == numberOfQuestions) {
+            showPopup(view);
+        }
 
     }
     public void showPopup(View v){
--- app/src/main/java/com/tobiasstrom/s331392mappe1comtobiasstrom/MainActivity.java
+++ app/src/main/java/com/tobiasstrom/s331392mappe1comtobiasstrom/MainActivity.java
@@ -1,8 +1,6 @@
 package com.tobiasstrom.s331392mappe1comtobiasstrom;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -11,46 +9,45 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.View;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
 
-    public void settland(String landskode) {
+    public void setLang(String prefix) {
         Resources res = getResources();
         DisplayMetrics dm = res.getDisplayMetrics();
         Configuration cf = res.getConfiguration();
+        //setLocale krever api lvl 17 (current definert api lvl er 15) Dette er en av forslagene til AndroidStudio
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            cf.setLocale(new Locale(landskode));
+            cf.setLocale(new Locale(prefix));
         }
         res.updateConfiguration(cf,dm);
     }
 
     @Override
+    //metoden tilbyr muligheten å kalle metoden settland etter at preferanse fragment er over
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode,resultCode,data);
         if(requestCode == 555) {
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-            settland(sharedPreferences.getString("languagePref",""));
+            setLang(sharedPreferences.getString("languagePref",""));
             recreate();
         }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d("ONcreate","hello");
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        settland(sharedPreferences.getString("languagePref", ""));
+        setLang(sharedPreferences.getString("languagePref", ""));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     public void btnStartGame(View view){
         startActivity(new Intent(this, GameActivity.class));
-
     }
 
     public void btnPreferences(View view){
@@ -59,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
         //startActivity(new Intent(this, PreferencesActivity.class));
         Intent intent = new Intent(this,SetPreferencesActivity.class);
         startActivityForResult(intent,555);
+         /*
+            dette vil kjøre metoden onActivityResult etter at preferanse activity blir ferdig
+            Det er noe som er nødvendig til å oppdatere språket.
+            Oncreate kjøres ikke hvis fragmentet preferanser er over.
+         */
 
     }
 
